204fc202ee243bac2ac7fe72e83c5a272fab4b90
Mac <mac@MacBook-Air.local>
2025-04-05 19:36:01 +0300

    Changes main logo

--- src/core/urls.py
+++ src/core/urls.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 
 def index(request):
     if request.user.is_authenticated:
-        return redirect('dashboard')
+        return redirect('questionnaires')
     else:
         return redirect('signin')
 
